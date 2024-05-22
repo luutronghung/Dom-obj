91a9863df4675211bbb92394303fcb423cca1add
HUNGDAV <luutronghung17@gmail.com>
2024-05-22 18:31:05 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+// document.getElementById(`submit`).addEventListener("click",(event)=>{
+//     event.preventDefault()
+//     console.log("hello button");
+// });
+
+function submitForm(e){
+    e.preventDefault();
+    console.log("hello world");
+}
